b70a7d697b318625b0fc89d1573c8fb74a205724
Youngjea Oh <ironmanciti@gmail.com>
2019-07-02 16:28:45 +0900

    'tuesday'

--- JS-google-sample2/script.js
+++ JS-google-sample2/script.js
@@ -3,21 +3,21 @@
 import {MnistData} from './data.js';
 
 async function showExamples(data) {
-  // Create a container in the visor
+  // visor 내의 container 생성
   const surface =
     tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});  
 
-  // Get the examples
-  const examples = data.nextTestBatch(20);
-  const numExamples = examples.xs.shape[0];
+  // 20 개의 test image 
+  const examples = data.nextTestBatch(20);    //examples.xs - tensor2d (20x784)
+  const numExamples = examples.xs.shape[0];   //20  
   
-  // Create a canvas element to render each example
+  // example 을 rendering 할 canvas element 생성
   for (let i = 0; i < numExamples; i++) {
     const imageTensor = tf.tidy(() => {
-      // Reshape the image to 28x28 px
+      // Reshape the image to 28x28x1 
       return examples.xs
-        .slice([i, 0], [1, examples.xs.shape[1]])
-        .reshape([28, 28, 1]);
+        .slice([i, 0], [1, examples.xs.shape[1]])   //slice([i,0], [1x784])
+        .reshape([28, 28, 1]); 
     });
     
     const canvas = document.createElement('canvas');
@@ -27,7 +27,7 @@ async function showExamples(data) {
     await tf.browser.toPixels(imageTensor, canvas);
     surface.drawArea.appendChild(canvas);
 
-    imageTensor.dispose();
+    imageTensor.dispose();  //Disposes imageTensor
   }
 }
 
@@ -38,24 +38,17 @@ function getModel() {
     const IMAGE_HEIGHT = 28;
     const IMAGE_CHANNELS = 1;  
 
-    // In the first layer of our convolutional neural network we have 
-    // to specify the input shape. Then we specify some parameters for 
-    // the convolution operation that takes place in this layer.
+    //CNN model define
     model.add(tf.layers.conv2d({
         inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
         kernelSize: 5,
         filters: 8,
         strides: 1,
         activation: 'relu',
-        kernelInitializer: 'varianceScaling'
+        kernelInitializer: 'varianceScaling'   //truncated normal distribution
     }));
-
-    // The MaxPooling layer acts as a sort of downsampling using max values
-    // in a region instead of averaging.  
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
 
-    // Repeat another conv2d + maxPooling stack. 
-    // Note that we have more filters in the convolution.
     model.add(tf.layers.conv2d({
         kernelSize: 5,
         filters: 16,
@@ -65,13 +58,9 @@ function getModel() {
     }));
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
 
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare
-    // it for input into our last layer. This is common practice when feeding
-    // higher dimensional data to a final classification output layer.
     model.add(tf.layers.flatten());
 
-    // Our last layer is a dense layer which has 10 output units, one for each
-    // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+    // output classes (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     const NUM_OUTPUT_CLASSES = 10;
     model.add(tf.layers.dense({
         units: NUM_OUTPUT_CLASSES,
@@ -79,9 +68,7 @@ function getModel() {
         activation: 'softmax'
     }));
 
-
-    // Choose an optimizer, loss function and accuracy metric,
-    // then compile and return the model
+    // model compile
     const optimizer = tf.train.adam();
     model.compile({
         optimizer: optimizer,
@@ -92,17 +79,20 @@ function getModel() {
     return model;
 }
 
+// model train
 async function train(model, data) {
     const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
     const container = {
       name: 'Model Training', styles: { height: '1000px' }
     };
+    // batch, epoch 마다 호출할 callback 함수
     const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
     
     const BATCH_SIZE = 512;
-    const TRAIN_DATA_SIZE = 5500;
-    const TEST_DATA_SIZE = 1000;
+    const TRAIN_DATA_SIZE = 5500;  // test 후 55000 으로 키워서 train
+    const TEST_DATA_SIZE = 1000;   // test 후 10000 으로 키움
   
+    // CNN 의 input shape - [num_examples, image_width, image_height, channels]
     const [trainXs, trainYs] = tf.tidy(() => {
       const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
       return [
@@ -112,9 +102,9 @@ async function train(model, data) {
     });
   
     const [testXs, testYs] = tf.tidy(() => {
-      const d = data.nextTestBatch(TEST_DATA_SIZE);
+      const d = data.nextTestBatch(TEST_DATA_SIZE);  // d = {xs, labels}
       return [
-        d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+        d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),  //d.xs - tensor2d (-1x784)
         d.labels
       ];
     });
@@ -144,21 +134,32 @@ async function run() {
 
 document.addEventListener('DOMContentLoaded', run);
 
-const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+const classNames = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function doPrediction(model, data, testDataSize = 500) {
   const IMAGE_WIDTH = 28;
   const IMAGE_HEIGHT = 28;
   const testData = data.nextTestBatch(testDataSize);
+  //testData.xs - shape [500, 784]
+  //testData.label - shape [500, 10]
   const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
-  const labels = testData.labels.argMax([-1]);
-  const preds = model.predict(testxs).argMax([-1]);
+  //testxs shape [500, 28, 28, 1]
+  const labels = testData.labels.argMax(1);        //last axis
+  const preds = model.predict(testxs).argMax(1);   //last axis
+
+  //   [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  //    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //    ...,
+  //    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  //    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
+  
+  //    [8, 0, 4, ..., 9, 4, 8]
 
   testxs.dispose();
   return [preds, labels];
 }
 
-
+//multiclass 의 class 별 accuracy 시각화
 async function showAccuracy(model, data) {
   const [preds, labels] = doPrediction(model, data);
   const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
@@ -168,6 +169,7 @@ async function showAccuracy(model, data) {
   labels.dispose();
 }
 
+// confusion matrix 시각화
 async function showConfusion(model, data) {
   const [preds, labels] = doPrediction(model, data);
   const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
